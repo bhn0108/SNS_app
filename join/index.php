dafd425d1e4c265b2e7564d35dfd15b3a5b9c4b5
bhn0108 <bhn0108@icloud.com>
2021-07-10 14:11:53 +0900

    add blank error

--- join/index.php
+++ join/index.php
@@ -1,7 +1,24 @@
 <?php
-if (isset($error['name']) && $_POST['name'] === 0) {
+if (!empty($_POST)) {
+	if (isset($error['name']) && $_POST['name'] === '') {
 	$error['name'] = 'blank';
+	}
+	if (isset($error['email']) && $_POST['email'] === '') {
+		$error['email'] = 'blank';
+	}
+	if (isset($error['password']) && strlen($_POST['password']) < 6) {
+		$error['password'] = 'length';
+	}
+	if (isset($error['password']) && $_POST['password'] === '') {
+		$error['password'] = 'blank';
+	}
+
+	if (empty($error)) {
+		header('Location： check.php');
+		exit();
+	}
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -26,17 +43,26 @@ if (isset($error['name']) && $_POST['name'] === 0) {
 	<dl>
 		<dt>ユーザー名<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="name" size="35" maxlength="255" value="" />
+        	<input type="text" name="name" size="35" maxlength="255" value="<?php if (isset($_POST['name'])) { echo htmlspecialchars($_POST['name'],ENT_QUOTES); } ?>" />
 			<?php if (isset($error['name']) && $error['name'] === 'blank'): ?>
 			<p class="error">* ユーザー名を入力してください</p>
 			<?php endif; ?>
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+        	<input type="text" name="email" size="35" maxlength="255" value="<?php if (isset($_POST['email'])) { echo htmlspecialchars($_POST['email'],ENT_QUOTES); } ?>" />
+			<?php if (isset($error['email']) && $error['email'] === 'blank'): ?>
+			<p class="error">* ユーザー名を入力してください</p>
+			<?php endif; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php if (isset($_POST['password'])) { echo htmlspecialchars($_POST['password'],ENT_QUOTES); } ?>" />
+			<?php if (isset($error['password']) && $error['password'] === 'length'): ?>
+			<p class="error">* パスワードは6文字以上で入力してください</p>
+			<?php endif; ?>
+			<?php if (isset($error['password']) && $error['password'] === 'blank'): ?>
+			<p class="error">* ユーザー名を入力してください</p>
+			<?php endif; ?>
         </dd>
 		<dt>写真など</dt>
 		<dd>
